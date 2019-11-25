a170e0b4c0f948fde13689c02d214c65ebee5707
Rabbia Hassan <rabbia.hassan@ihsinformatics.com>
2019-11-25 09:46:28 +0500

    Added Async annotation to datawarehouse service

--- aahung-aagahi/src/main/java/com/ihsinformatics/aahung/aagahi/WebConfig.java
+++ aahung-aagahi/src/main/java/com/ihsinformatics/aahung/aagahi/WebConfig.java
@@ -19,6 +19,9 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,6 +41,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebSecurity
 @EnableWebMvc
+@EnableAsync
 public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Autowired
@@ -64,6 +68,16 @@ public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcCon
     public BCryptPasswordEncoder passwordEncoder() {
 	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 	return encoder;
+    }
+    
+    @Bean("threadPoolTaskExecutor")
+    public TaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(1000);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setThreadNamePrefix("Async-");
+        return executor;
     }
 
     /**
