7e44480fd47bb53c863449ff3d47a93ce845c4d7
Rabbia Hassan <rabbia.hassan@ihsinformatics.com>
2020-01-01 14:05:40 +0500

    void and restore controller added

--- aahung-aagahi/src/main/java/com/ihsinformatics/aahung/aagahi/web/ParticipantController.java
+++ aahung-aagahi/src/main/java/com/ihsinformatics/aahung/aagahi/web/ParticipantController.java
@@ -29,17 +29,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.ihsinformatics.aahung.aagahi.dto.LocationDesearlizeDto;
 import com.ihsinformatics.aahung.aagahi.dto.ParticipantDesearlizeDto;
+import com.ihsinformatics.aahung.aagahi.model.FormData;
 import com.ihsinformatics.aahung.aagahi.model.Location;
 import com.ihsinformatics.aahung.aagahi.model.Participant;
 import com.ihsinformatics.aahung.aagahi.service.DonorService;
@@ -96,10 +99,31 @@ public class ParticipantController extends BaseController {
 
     @ApiOperation(value = "Delete Participant")
     @DeleteMapping("/participant/{uuid}")
-    public ResponseEntity<?> deleteParticipant(@PathVariable String uuid) {
+    public ResponseEntity<?> deleteParticipant(@PathVariable String uuid, @RequestParam("reasonVoided")String reasonVoided) {
 	LOG.info("Request to delete participant: {}", uuid);
 	try {
-	    service.deleteParticipant(service.getParticipantByUuid(uuid));
+		Participant participant = uuid.matches(RegexUtil.UUID) ? service.getParticipantByUuid(uuid)
+				: service.getParticipantById(Integer.parseInt(uuid));
+		if(participant == null)
+			return noEntityFoundResponse(uuid);
+		participant.setReasonVoided(reasonVoided);
+	    service.voidParticipant(participant);
+	} catch (Exception e) {
+	    return exceptionFoundResponse("Reference object: " + uuid, e);
+	}
+	return ResponseEntity.noContent().build();
+    }
+    
+    @ApiOperation(value = "Restore Participant")
+    @PatchMapping("/participant/{uuid}")
+    public ResponseEntity<?> unvoidParticipant(@PathVariable String uuid) {
+	LOG.info("Request to restore form data: {}", uuid);
+	try {
+		Participant participant = uuid.matches(RegexUtil.UUID) ? service.getParticipantByUuid(uuid)
+				: service.getParticipantById(Integer.parseInt(uuid));
+		if(participant == null)
+			return noEntityFoundResponse(uuid);
+	    service.unvoidParticipant(participant);
 	} catch (Exception e) {
 	    return exceptionFoundResponse("Reference object: " + uuid, e);
 	}
